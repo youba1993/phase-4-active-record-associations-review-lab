61dbad55849f45c99a846d0fed0bdaa595b441da
youbaidjis <youba.etud@gmail.com>
2022-08-26 14:32:54 -0700

    commit tree 9ecf7e7bc4a6c32589c830592a3a8aa4f1f6ed75

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,5 @@
 class Passenger < ApplicationRecord
+    has_many :rides
+    has_many :taxis, through: :rides
+
 end
--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      t.integer     "taxi_id", null: false
+      t.integer     "passenger_id", null: false
 
       t.timestamps
     end
